d05e374908e21433fa72adfc08c4f2ee88f73a78
Chidloski <s.chidlow07@gmail.com>
2024-06-29 19:18:43 +0100

    adding api

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    ID = models.IntegerField(max_length = 11, primary_key = True)
+    ID = models.IntegerField(primary_key = True)
     Name = models.CharField(max_length = 255)
     No_of_guests = models.IntegerField(max_length = 6)
     BookingDate = models.DateTimeField()
 
 class Menu(models.Model):
-    ID = models.IntegerField(max_length = 5, primary_key = True)
+    ID = models.IntegerField(primary_key = True)
     Title = models.CharField(max_length = 255)
     Price = models.DecimalField(max_digits = 10, decimal_places = 2)
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics, response, status, viewsets
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    '''def get(self, request):
+        return response.Response({"message": "menu"}, status.HTTP_200_OK)
+    
+    def post(self, request):
+        return response.Response({"message": "new menu item added"}, status.HTTP_201_CREATED)'''
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+
+
+
+    
